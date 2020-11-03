746c82ca3f53b93ff045479d942d19935a89cf29
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2020-11-04 01:02:07 +0200

    Incomplete UI and database code, parser file with todo.

---
+++ db.py
@@ -0,0 +1,26 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_table(conn, statement):
+    """Create a table from the statement.
+    :param conn: Connection object
+    :param statement: a CREATE TABLE statement
+    """
+    try:
+        c = conn.cursor()
+        c.execute(statement)
+    except Error as e:
+        print(e)
+
+try:
+    conn = sqlite3.connect("recipes.db")
+    c = conn.cursor()
+except Error as e:
+    print(e)
+
+if conn is not None:
+    # TODO: Create a recipe table.
+    pass
+else:
+    print("Error! No database connection.")
---
+++ parser.py
@@ -0,0 +1,2 @@
+# TODO: Parse a file with recipes and parse each of them into a dict(?)
+#       probably using some regexp. Then either return them or upload to DB.
